90c9741b3ec27ee58f8c9d52c2107c19d82c53d2
David Siemionko <dsiemionko4406@stu.d214.org>
2020-11-06 15:20:42 -0500

    Initial Commit

---
+++ UIViewsLecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIViewsLecture
+//
+//  Created by user180860 on 11/6/20.
+//  Copyright © 2020 user180860. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
